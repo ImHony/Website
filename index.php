76767c771844b3fe0a9a5d5e8976f733265323a7
ImHony <gabrielgabi101@gmail.com>
2022-05-19 15:08:05 +0300

    Added

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php
+    include 'templates/head.html';
+   ?>
+</head>
+<body>
+ <?php
+    include 'templates/navbar.html';
+   ?>
+  
+<div class="container">
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-label="Slide 1" aria-current="true"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class=""></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="2000" style="background: coral;">
+          <img src="images/gardenslide1.jpg" class="d-block w-100"> </img>
+        <div class="containerCarousel">
+          <div class="carousel-caption">
+            xd
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2000" style="background: lightgreen;">
+        <img src="images/gardenslide2.jpg" class="d-block w-100"> </img>
+        <div class="containerCarousel">
+          <div class="carousel-caption">
+            <p>xd cu p</p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item" data-bs-interval="2000" style="background:slateblue;">
+        <img src="images/gardenslide3.jpg" class="d-block w-100"> </img>
+        <div class="containerCarousel">
+          <div class="carousel-caption">
+            <h1>xd cu h1</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>
+<div class="container" style="background-color: #43b14b;"><h1 style="font-size: 25px; text-align: center; height: 50px; padding: 10px 0;">Despre noi</h1></div>
+<div class="container">
+  <span style="float: left; display: inline-block;">
+    <p style="max-width: 500px; margin-left: 10px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam magna tellus, dapibus vel dapibus a, aliquam at lectus. Pellentesque sodales elit sed nulla feugiat sollicitudin. Nulla rhoncus lectus non elit congue rutrum. Praesent aliquam consequat sollicitudin. Pellentesque ut vulputate magnaLorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam magna tellus, dapibus vel dapibus a, aliquam at lectus. Pellentesque sodales elit sed nulla feugiat sollicitudin. Nulla rhoncus lectus non elit congue rutrum. Praesent aliquam consequat sollicitudin. Pellentesque ut vulputate magna.
+    </p>
+  </span>
+  <span style="float: right;display: inline-block">
+  <p style="max-width: 500px; margin-left: 10px;">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam magna tellus, dapibus vel dapibus a, aliquam at lectus. Pellentesque sodales elit sed nulla feugiat sollicitudin. Nulla rhoncus lectus non elit congue rutrum. Praesent aliquam consequat sollicitudin. Pellentesque ut vulputate magnaLorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam magna tellus, dapibus vel dapibus a, aliquam at lectus. Pellentesque sodales elit sed nulla feugiat sollicitudin. Nulla rhoncus lectus non elit congue rutrum. Praesent aliquam consequat sollicitudin. Pellentesque ut vulputate magna.
+</p>
+</span>
+</div>
+
+<div class="container" style=" clear: both; display: block; background-color: #43b14b;"><h1 style="font-size: 25px; text-align: center; height: 50px; padding: 10px 0;">Cu ce ne ocupam</h1></div>
+
+
+<div class="container">
+    <div class="row"  style="margin-right:0px; margin-left: 0px;">
+        <div class="col-md-3" style="background-color: mediumorchid; width: 330px; height: 300px; margin: 0 auto; margin-bottom: 5px; margin-top: 5px;">
+        xd
+        </div>
+        <div class="col-md-3" style="background-color: coral; width: 330px; height: 300px;  margin: 0 auto; margin-bottom: 5px;  margin-top: 5px;">
+           xd
+        </div>          
+        <div class="col-md-3" style="background-color: forestgreen; width: 330px; height: 300px;  margin: 0 auto; margin-bottom: 5px;  margin-top: 5px;">
+        xd
+        </div>          
+        <div class="col-md-3" style="background-color: aquamarine; width: 330px; height: 300px;  margin: 0 auto; margin-bottom: 5px;  margin-top: 5px;">
+          xd
+        </div>          
+    </div>
+</div>
+
+<div class="container" style="font-size:25px ;"> 
+  <div class="row" style="margin-right:0px; margin-left: 0px;">
+      <div class="col-md-4" style="background: #6ac870">
+               <i class="bi bi-telephone" ></i>Telefon:
+               <br>
+               0712345678
+               <br>
+               0712345678
+               <br>
+                <i class="bi bi-envelope" ></i>Email:
+               <br>
+               contactnewgarden@hotmail.com
+               <br>
+               contactnewgarden@gmail.com
+               <br>
+      </div>
+
+      <div class="col-md-4" style="background: #6ac870;">
+            
+          <span style="float:left;">
+            <ul style="list-style: none; display: inline-block; padding: 0px ;">
+              <li>Program:</li>
+              <li>Luni </li>
+              <li>Marti</li>
+              <li>Miercuri</li>
+              <li>Joi </li>
+              <li>Vineri</li>
+              <li>Sambata </li>
+              <li>Duminica</li>
+            </ul>
+          </span>
+            <span style="float:right;">
+            <ul style="list-style: none;  padding: 0px; display: inline-block;">
+              <li>⠀</li>
+              <li>08:00-20:00</li>
+              <li>08:00-20:00</li>
+              <li>08:00-20:00</li>
+              <li>08:00-20:00</li>
+              <li>08:00-20:00</li>
+              <li>08:00-12:00</li>
+              <li>Inchis</li>
+            </ul>
+          </span>
+      </div>
+
+      <div class="col-md-4" style="background: #6ac870;">
+        <i class="bi bi-geo-alt"></i>
+      </div>
+
+
+</div>
+</div>
+           
+</body>
+</html>
