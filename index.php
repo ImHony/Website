294beb6f6fed72a0dffbabea8a630f1a36727225
ImHony <gabrielgabi101@gmail.com>
2022-05-25 12:20:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,13 @@
 
 <div class="flexible-container" style="background-color: #2EC4B6;"><h1 style="font-size: 25px; text-align: center; height: 50px; padding: 10px 0; border-radius: 100px;">Despre noi</h1></div>
 
-<div class="flexible-container" style="background-color: #2EC4B6;"><h1 style="font-size: 25px; text-align: center; height:50px; padding: 10px 0;"><i class="fa-solid fa-lightbulb" style="margin-right: 5px;"></i>In your ideas we trust<i class="fa-solid fa-lightbulb" style="margin-left: 5px;"></i></h1></div>
-
+<div class="flexible-container" style="background-color: #2EC4B6;"><h1 style="font-size: 25px; text-align: center; height:50px; padding: 10px 0;"><i class="fa-solid fa-lightbulb" style="margin-right: 5px;"></i>In your ideas we trust<i class="fa-solid fa-lightbulb" style="margin-left: 5px;"></i></h1>
+</div>
+   <p style="color: white; text-align: center; font-size: 18px;" class="container">In your ideas we trust este un eveniment lunar dedicat studenilor cat si elevilor!<br>In fiecare luna abordam o tema diferita, tot ce trebuie sa faci este sa vii cu o idee sau chiar un mic proiect.
+</p>
+<p style="text-align: center; color: #2EC4B8; text-decoration: underline; margin-top: -12px;  font-size: 20px;"  class="container"><i class="fa-solid fa-recycle"></i> Tema de luna aceasta este reciclarea plasticului. <i class="fa-solid fa-recycle"></i></p> 
+<p style="text-align: center; color: white;  margin-top: -12px; font-size: 18px;"  class="container">Elaboreaza desene sau reprezentari 3D, machete, statistici sau diagrame prin care sa iti prezinti ideea. De asemenea, te rugam sa adaugi si un mini eseu.</p>
+<p style="text-align: center; color: white;  margin-top: -12px; font-size: 18px;"  class="container">Pentru incarcarea proiectului va rugam sa folositi adresa de e-mail <span style="text-decoration: underline;">3dfarmideas@contact.com</span></p>
 <div class="flexible-container" style=" clear: both; display: block; background-color: #2EC4B6;"><h1 style="font-size: 25px; text-align: center; height: 50px; padding: 10px 0;">Cele mai folosite filamente</h1></div>
 
 
